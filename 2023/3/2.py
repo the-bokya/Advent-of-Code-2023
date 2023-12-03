34cb7898a9cde52044fbc63f8aefc31700d974e0
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2023-12-03 16:20:36 +0530

    Remove mistake where two numbers with same value but different locations are counted as one

--- 2023/3/2.py
+++ 2023/3/2.py
@@ -9,7 +9,7 @@ with open("input") as f:
         j = 0
         grid_row = []
         for part in current:
-            grid_row.extend([part]*len(part))
+            grid_row.extend([[i, j, part]]*len(part))
             if re.match("\*", part):
                 stars.append([i, j, part])
             j += len(part)
@@ -20,15 +20,16 @@ print(nums)
 print(grid, len(grid), len(grid[0]))
 """
 def finder(i, j, num):
-    gears = set()
+    gears = []
     for x in range(max(i - 1, 0), min(len(grid), i+2)):
         for y in range(max(0, j - 1), min(len(grid[0]), j + 2)):
                 #print(x, y, grid[x][y])
-                if re.match("[\d]", grid[x][y]):
+                if re.match("[\d]", grid[x][y][2]):
                     print(grid[x][y])
-                    gears.add(grid[x][y])
+                    if grid[x][y] not in gears:
+                        gears.append(grid[x][y])
     if len(gears) == 2:
-        return reduce(lambda x, y: int(x)*int(y), list(gears))
+        return reduce(lambda x, y: int(x[2])*int(y[2]), list(gears))
     return 0
 count = 0
 for star in stars:
