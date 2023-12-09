8e24b004c86360a3620cd8373529a4853a2279a7
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2023-12-09 10:47:39 +0530

    Second of day nine

---
+++ 2023/2.py
@@ -0,0 +1,20 @@
+numses = []
+with open("input") as f:
+    for i in f:
+        i = i.removesuffix("\n")
+        numses.append([int(j) for j in i.split(" ")])
+
+print(numses)
+
+def oasis(nums):
+    if all(map(lambda x: x == 0, nums)):
+        return 0
+    diffs = []
+    prev = nums[0]
+    for i in nums[1:]:
+        diffs.append(i - prev)
+        prev = i
+    
+    return nums[0] - oasis(diffs) 
+
+print(sum(list(map(oasis, numses))))
